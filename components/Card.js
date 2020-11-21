289ab56cbd98ffe666b87d120ba26adfb56e0e3b
ashandileo <ashandileonadi2001@gmail.com>
2020-11-21 21:29:41 +0700

    get movies data and crete Card component

---
+++ components/Card.js
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled'
+
+const Card = ({ movie }) => {
+  const { API_URL } = process.env
+
+  return (
+    <CardStyled>
+      <div className="poster">
+        <img src={API_URL + movie.poster.url} alt={movie.poster.name} />
+      </div>
+      <div className="body">
+        <h3>{movie.title}</h3>
+        <p dangerouslySetInnerHTML={{__html: movie.description}} />
+      </div>
+    </CardStyled>
+  )
+}
+
+const CardStyled = styled.div`
+  width: 400px;
+  border: 1px solid #ccc;
+  margin-top: 50px;
+  border-radius: 20px;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+
+  .poster {
+    height: 300px;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+
+  .body {
+    padding: 20px;
+
+    h3 {
+      margin-bottom: 20px;
+    }
+  
+    p {
+      color: #666;
+      line-height: 1.5;
+    }
+  }
+`
+
+export default Card
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,32 @@
-const Home = () => {
+import Card from 'components/Card'
+import fetch from 'isomorphic-unfetch'
+import { camelizeKeys } from 'humps'
+
+const Home = ({ movies }) => {
+  console.log("Home -> movies", movies)
   return (
     <div className="container">
-      <h1>This is our front page</h1>
+      {
+        movies.map(movie => <Card
+          key={movie.id}
+          movie={movie}
+        />)
+      }
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/movies`)
+  const data = await res.json()
+
+  return {
+    props: {
+      movies: camelizeKeys(data)
+    }
+  }
 }
 
 export default Home
