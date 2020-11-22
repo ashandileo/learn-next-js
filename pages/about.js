9ebdb6d8857fc0ceabe167288f75765793f2afbb
ashandileo <ashandileonadi2001@gmail.com>
2020-11-22 15:29:45 +0700

    use getStaticProps to get about page data

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import { NextSeo } from 'next-seo'
+import { Flex, Box } from 'reflexbox'
 
-const About = () => {
+const About = ({ page }) => {
   const SEO = {
-    title: 'About',
+    title: page.title,
     description: 'Just your normal About Page',
+
     openGraph: {
-      title: 'About',
+      title: page.title,
       description: 'Just your normal About Page',
     },
   }
@@ -13,9 +15,25 @@ const About = () => {
   return (
     <>
       <NextSeo {...SEO} />
-      <h1>I'm an About Page</h1>
+      <Box variant="container">
+        <Box as="h2" my={40}>{page.title}</Box>
+        <p dangerouslySetInnerHTML={{ __html: page.content }} />
+      </Box>
     </>
   )
+}
+
+export const getStaticProps = async () => {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/pages/1`)
+  const data = await res.json()
+
+  return {
+    props: {
+      page: data
+    }
+  }
 }
 
 export default About
